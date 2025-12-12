6e5bc1bff98b1c2289b0dc81f1758fecd0ec8110
AadhavSivakumar <hiaadhav@gmail.com>
2025-12-11 20:26:16 -0500

    part 4 no modifications

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -105,3 +105,9 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     torque_limits = 100.0  # Max torque
 
     base_height_min = 0.20  # Terminate if base is lower than 20cm
+
+    observation_space = 48 + 4  # Added 4 for clock inputs
+
+    raibert_heuristic_reward_scale = -10.0
+    feet_clearance_reward_scale = -30.0
+    tracking_contacts_shaped_force_reward_scale = 4.0
